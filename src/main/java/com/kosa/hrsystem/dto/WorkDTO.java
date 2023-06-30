8179f8cb77c7a6743f01378d84f6460fdaa44068
jkl <jkl02138@gmail.com>
2023-06-30 15:25:20 +0900

    feat WorkDTO 추가
    WorkDTO 추가하였습니다.

---
+++ src/main/java/com/kosa/hrsystem/dto/WorkDTO.java
@@ -0,0 +1,26 @@
+package com.kosa.hrsystem.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class WorkDTO {
+	private int work_num;
+	private int emp_num;
+	private int bsns_num;
+	private String work_name;
+	private int pay;
+	private int min_working_day;
+	private int weekly_holiday;
+	private String remarks;
+	private char mon;
+	private char tue;
+	private char wed;
+	private char thu;
+	private char fri;
+	private char sat;
+	private char sun;
+}
