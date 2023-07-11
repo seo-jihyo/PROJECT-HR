32d82f39f0a2d1bafefe52cb6aef6155f51b239a
Song014 <ghgsb6200@gmail.com>
2023-07-11 19:01:29 +0900

    fix: email

--- src/main/java/com/kosa/hrsystem/utils/NaverSMTP.java
+++ src/main/java/com/kosa/hrsystem/utils/NaverSMTP.java
@@ -20,6 +20,7 @@ public class NaverSMTP {
 	
 	private final Properties serverInfo; // 서버정보
 	private final Authenticator auth; 	 // 인증정보
+	private static Properties props;
 	
 	public NaverSMTP() {
 		serverInfo = new Properties();
@@ -33,7 +34,7 @@ public class NaverSMTP {
 		serverInfo.put("mail.smtp.socketFactory.fallback", "false");
 		serverInfo.put("mail.smtp.ssl.protocols", "TLSv1.2");
 		String resource = "email.properties";
-		Properties props = new Properties();
+		props = new Properties();
 		try {
 			Reader reader = Resources.getResourceAsReader(resource);
 			props.load(reader);
@@ -58,7 +59,7 @@ public class NaverSMTP {
 		
 		// 2. 메시지 작성
 		MimeMessage msg = new MimeMessage(session); 
-		msg.setFrom(new InternetAddress(mailInfo.get("from"))); // 보내는 사람
+		msg.setFrom(new InternetAddress(props.getProperty("id"))); // 보내는 사람
 		msg.addRecipient(Message.RecipientType.TO, new InternetAddress(mailInfo.get("to"))); // 받는 사람
 		msg.setSubject(mailInfo.get("subject")); // 제목
 		msg.setContent(mailInfo.get("content"),mailInfo.get("format")); // 내용
